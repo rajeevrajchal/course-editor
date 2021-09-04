b38e71560f6e5b53ac6ef5d97861e2f29153705f
Rajeev Rajchal <41408167+rajeevrajchal@users.noreply.github.com>
2021-09-04 20:12:59 +0545

    update the redux reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,11 @@ import {
     applyMiddleware
 } from 'redux';
 import thunk from 'redux-thunk'
+import rootReducer from "./reducers";
 const middleware = [thunk]
 
 const store = createStore(
-    reducer,
+    rootReducer,
     applyMiddleware(...middleware)
 )
 export default store
